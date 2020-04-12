ecac23a5e5fc9e43da3d61550381535b96b13686
Igor <1c.cloud@mail.ru>
2020-04-12 16:42:20 +0500

    тест

---
+++ for.py
@@ -0,0 +1,4 @@
+students_scores =[1,21,19,6,5]
+
+for score in students_scores :
+    print(score)
---
+++ homelesson6.py
@@ -0,0 +1,7 @@
+while True:
+    user_say = input('Как дела?: ')
+    if user_say =='Хорошо':
+       # print('Ну пока')
+        break
+   # else:
+    #    print('Сам ты {}'.format(user_say))
